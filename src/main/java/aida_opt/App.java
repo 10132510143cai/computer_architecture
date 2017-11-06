7b137d2b83280a1790c502aa902dc92d8e3953b0
10132510143cai <1067694145@qq.com>
2017-11-06 21:43:46 +0800

    First work initialized

---
+++ src/main/java/aida_opt/App.java
@@ -0,0 +1,13 @@
+package aida_opt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
